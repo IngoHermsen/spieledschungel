02817510d4e164866774ec7bae4a8a291bf8a39a
IngoHermsen <you@example.com>
2026-01-28 19:21:34 +0100

    Style: mobile navigation sticky

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Header } from './components/header/header';
+import { Navigation } from './components/navigation/navigation';
 
 @Component({
   selector: 'app-root',
-  imports: [Header],
+  imports: [Header, Navigation],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
