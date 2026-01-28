7813a28250f55e0806b879b5e05ae0a7c3ca2acd
IngoHermsen <you@example.com>
2026-01-28 19:16:05 +0100

    Style: navigation responsive

--- src/app/components/navigation/navigation.ts
+++ src/app/components/navigation/navigation.ts
@@ -9,20 +9,18 @@ import { ViewService } from '../../services/view-service';
 })
 export class Navigation implements OnInit {
   viewService = inject(ViewService);
-  expandableMenu: boolean;
-  showMobileMenu: boolean = false;
+  navIsOpen: boolean = false;
 
   constructor() {
-    this.expandableMenu = this.viewService.isMobileView;
+
   }
 
   ngOnInit(): void {
     
   }
 
-  toggleMobileMenu() {
-    this.showMobileMenu = !this.showMobileMenu;
+ toggleNav() {
+    this.navIsOpen = !this.navIsOpen;
   }
-
 
 }
--- src/app/services/view-service.ts
+++ src/app/services/view-service.ts
@@ -4,13 +4,9 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class ViewService {
-  // Mobile View Controls
 
-  mobileViewThreshold: number = 800;
-  isMobileView: boolean;
 
   constructor() {
-    console.log('view service');
-    this.isMobileView = window.innerWidth <= this.mobileViewThreshold;
+ 
   }
 }
