e1fd635b833bc74547724df660901da04a5eeb71
IngoHermsen <you@example.com>
2026-04-19 00:36:53 +0200

    responsive media conrols

--- src/app/components/media-controls/media-controls.ts
+++ src/app/components/media-controls/media-controls.ts
@@ -1,13 +1,15 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { AudioService } from '../../services/audio-service';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-media-controls',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './media-controls.html',
   styleUrl: './media-controls.scss',
 })
 export class MediaControls {
+  @Input() modalVariant: boolean = false;
   audioService = inject(AudioService);
 
   stopPropagation(event: Event) {
--- src/app/components/modal/modal.ts
+++ src/app/components/modal/modal.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { MediaControls } from '../media-controls/media-controls';
 import { ViewService } from '../../services/view-service';
+import { KeyControlService } from '../../services/key-control';
 
 @Component({
   selector: 'app-modal',
@@ -10,5 +11,7 @@ import { ViewService } from '../../services/view-service';
 })
 export class Modal {
   public viewService = inject(ViewService);
+  private keyControlService = inject(KeyControlService);
+
 
 }
